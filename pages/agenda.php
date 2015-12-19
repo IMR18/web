0ea00583ba74b6c83cf1cb60f82a450920106152
zaza35 <zaid172009@gmail.com>
2015-12-19 20:54:35 +0100

    fix bugs2

--- pages/agenda.php
+++ pages/agenda.php
@@ -1,9 +1,7 @@
 <h3 style='float:left;'>Agenda</h3><br>
 <a class="bPopupModal-src" modal-target="ajouterUneTache"><div style='float:left;' class="btn btn-primary pull-right">Ajouter une tâche</div></a>
-<div>
   <?php
   include("../functions/functions.php");
   	loadDashBoard();
-    include ('pages/addtask.php');
+    include_once('../pages/addtask.php');
       ?>
-</div>
