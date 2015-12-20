04523f225130a74ca64797d691f3bd7d9285091a
zaza35 <zaid172009@gmail.com>
2015-12-21 00:14:34 +0100

    forcage UTF8 dans php/mysql/json

--- php/functions.php
+++ php/functions.php
@@ -1,10 +1,12 @@
 <?php
-
+header('Content-Type: text/html; charset=utf-8');
+header('Content-Type: application/json');
 function PDO($host="imr18.fr",$user="sqlimr",$pass="P42A0;ch-A3_8",$db="imr"){
 	try {
 		$db = new PDO("mysql:host=$host;dbname=$db",$user,$pass);
 		$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 		$db->exec("SET CHARACTER SET utf8");
+		$db->exec("SET NAMES utf8");
 	}
 	catch (PDOException $e){
 		die();
@@ -14,6 +16,6 @@ function PDO($host="imr18.fr",$user="sqlimr",$pass="P42A0;ch-A3_8",$db="imr"){
 
 
 
-//vos fonctions generiques ici 
+//vos fonctions generiques ici
 
 ?>
