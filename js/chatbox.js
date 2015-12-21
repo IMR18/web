0de9d64f3c36d20ff84e1a6d9f2978613479d730
iN33L <charpentier.pierre@outlook.com>
2015-12-21 23:08:31 +0100

    Timestamp dans la chatbox + prob label

--- js/chatbox.js
+++ js/chatbox.js
@@ -1,20 +1,32 @@
 $(document).ready(function() {
-  var ref = new Firebase("https://imr18.firebaseio.com/chat/");
+  var ref = new Firebase("https://imr18.firebaseio.com/chat/messages");
   var UUID = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = crypto.getRandomValues(new Uint8Array(1))[0]%16|0, v = c == 'x' ? r : (r&0x3|0x8);
     return v.toString(16);
 });
 
   var highlightInterval = null;
-  isChatboxOpen = false;
+  var isChatboxOpen = false;
+  var lastTimestamp = 0;
 
-  ref.on("value", function(snapshot) {
+  ref.on("child_added", function(snapshot) {
     var data = snapshot.val();
-    var p = $("<p></p>");
+    console.log("Last: ", lastTimestamp, " CUrrent: ", data.timestamp);
+    if (lastTimestamp == null || (data.timestamp - lastTimestamp) > 1000*60*5) {
+      var timestamp = $("<p></p>");
+      var date = new Date(data.timestamp);
+      timestamp.text(date.getHours() + ":" + date.getMinutes() + " - " + date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear());
+      timestamp.addClass("timestamp");
+      $("#chatboxTextarea div").append(timestamp);
+    }
+
+    lastTimestamp = data.timestamp;
+
+    var message = $("<p></p>");
     var c = (data.uuid == UUID) ? "self":"other";
-    p.addClass(c);
-    p.text(data.message);
-    $("#chatboxTextarea div").append(p);
+    message.addClass(c);
+    message.text(data.message);
+    $("#chatboxTextarea div").append(message);
     $('#chatboxTextarea div').animate({
         scrollTop: $('#chatboxTextarea div').get(0).scrollHeight},
         1000);
@@ -51,9 +63,13 @@ $(document).ready(function() {
     e.preventDefault();
     var msg = $("#chatboxInput").val();
     if (msg.length) {
-      ref.set({uuid: UUID, message: msg});
+      ref.push({uuid: UUID, message: msg, timestamp: Firebase.ServerValue.TIMESTAMP});
     }
     $("#chatboxInput").val("");
+  });
+
+  $("#chatboxSubmit").click(function() {
+    $("#chatboxFooter").submit();
   });
 
   function chatboxHighlight() {
