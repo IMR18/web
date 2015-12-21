83e82b3e78694062dffae0f72488c2c655fb0789
jjth <jjth01@gmail.com>
2015-12-21 20:49:38 +0100

    Utilise la même connexion à la bdd plusieurs fois de suite au lieu d'en réouvrir une à chaque requête.

--- php/functions.php
+++ php/functions.php
@@ -2,6 +2,9 @@
 header('Content-Type: text/html; charset=utf-8');
 header('Content-Type: application/json');
 function PDO($host="imr18.fr",$user="sqlimr",$pass="P42A0;ch-A3_8",$db="imr"){
+	global $pdo_Database_Set;
+	if(isset($pdo_Database_Set)) return $pdo_Database_Set;
+	
 	try {
 		$db = new PDO("mysql:host=$host;dbname=$db",$user,$pass);
 		$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
@@ -11,6 +14,7 @@ function PDO($host="imr18.fr",$user="sqlimr",$pass="P42A0;ch-A3_8",$db="imr"){
 	catch (PDOException $e){
 		die();
 	}
+	$pdo_Database_Set=$db;
 	return $db;
 }
 
